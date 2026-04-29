4a4d8de5002cc261e8149eb0365ed93d4243de40
rajendrachy <chyrajendra32@gmail.com>
2026-04-29 13:20:15 +0530

    feat: mobile layout fixes and poll implementation

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // const BASE_URL =
 //   import.meta.env.MODE === "development"
 //     ? "http://localhost:5001/api"
-//     : "https://slack-backend-lime.vercel.app/api";
+//     : "https://nexus-backend-lime.vercel.app/api";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
